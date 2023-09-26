692192e5134b8acfe0768a985e39b9900c851403
Swasti008 <144793835+Swasti008@users.noreply.github.com>
2023-09-26 12:41:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// Challenge 1: Store the string "I am a" in one variable and store the other string "Kalvian" in other variable and concatenate both the string to get the output as "I am a Kalvain" using "+" operator.
+
+let a="I am a";
+let b="Kalvian";
+console.log(a+" "+b);
+
+// Challenge 2: Store the given string in a variable and print the same in console. String: If you fail, never give up because FAIL means "First Attempt In Learning".
+
+let c ="If you fail, never give up because FAIL means \"First Attempt In Learning\"."
+console.log(c);
+
+// Challenge 3: Store a string in a variable and display the length of the string.
+
+let d= "happy";
+console.log(d.length);
+
+// Challenge 4: Store a string in a variable and convert it into uppercase.
+
+let e="coding";
+console.log(e.toUpperCase());
+
+//Challenge 5: Store the string "I love programming" in a variable and replace the word programming with "JavaScript programming".
+
+let f="I love programming";
+console.log(f.replace("programming","JavaScript programming"));
+
+// Challenge 6: Store a string in variable and print the string in console 3 times using repeat method.
+
+let g="swasti";
+console.log(g.repeat(3));
+
+//Challenge 7: Store the string as "I am a Kalvian" in a variable and convert the string into array and print the array in console.
+
+let h="I am a Kalvian";
+console.log(h.split(" "));
+
+// Challenge 8: Store the given string "Time and Tide wait for none" in a variable and using indexOf method find the index of "Ti" and print the same in console.
+ 
+let i="Time and Tide wait for none";
+console.log(i.indexOf("Ti"));
+
+// Challenge 9: Store the given string "When life gives you lemons make lemonade" in a variable and find whether it includes "lemon".
+
+let j="When life gives you lemons make lemonade";
+console.log(j.includes("lemon"));
+
+// Challenge 10: Store the given string "You must be the change you wish to see in the world." in a variable and use slice to console log the word "change" and use substring method to console log the word "world".
+
+let k="You must be the change you wish to see in the world.";
+console.log(k.slice(16,23));
+console.log(k.substring(46,51));
+
+
